39b87ba0920c576a41c9d11776487563ded9a94a
dudlf <dudlf016@gmail.com>
2025-06-26 20:37:22 +0900

    feat: 올바르지 않은 경로일 경우 예외 처리

--- roadeye-api/roadeye-api-hub/src/main/java/org/re/web/advice/MdtExceptionAdvice.java
+++ roadeye-api/roadeye-api-hub/src/main/java/org/re/web/advice/MdtExceptionAdvice.java
@@ -10,11 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class MdtExceptionAdvice {
-    @Priority(Ordered.HIGHEST_PRECEDENCE)
+    @Priority(Ordered.HIGHEST_PRECEDENCE + 100)
     @ExceptionHandler(AppException.class)
     public Object handleMdtException(AppException e) {
         var code = e.getCode();
@@ -30,12 +31,20 @@ public class MdtExceptionAdvice {
             .body(new BaseMdtResponse(MdtLogExceptionCode.DATA_PROCESSING_ERROR));
     }
 
-    @Priority(Ordered.HIGHEST_PRECEDENCE)
+    @Priority(Ordered.HIGHEST_PRECEDENCE + 100)
     @ExceptionHandler(Exception.class)
     public Object handleException(Exception e) {
         log.error("Unhandled exception: {}", e.getMessage(), e);
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(new BaseMdtResponse(MdtLogExceptionCode.INTERNAL_SERVER_ERROR));
+    }
+
+    @Priority(Ordered.HIGHEST_PRECEDENCE)
+    @ExceptionHandler(NoResourceFoundException.class)
+    public Object handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new BaseMdtResponse(MdtLogExceptionCode.INVALID_ACCESS_PATH));
     }
 }
