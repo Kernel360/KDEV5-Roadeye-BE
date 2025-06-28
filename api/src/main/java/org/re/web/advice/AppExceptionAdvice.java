59d6ef6f2d739f56d38db641882cb8a1e3b7a2d1
dudlf <dudlf016@gmail.com>
2025-06-28 17:20:10 +0900

    fix: hub 모듈 RestControllerAdvice 우선순위 문제

--- api/src/main/java/org/re/web/advice/AppExceptionAdvice.java
+++ api/src/main/java/org/re/web/advice/AppExceptionAdvice.java
@@ -1,14 +1,21 @@
 package org.re.web.advice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.re.common.api.payload.ErrorResponse;
 import org.re.common.exception.AppException;
 import org.re.common.exception.CommonAppExceptionCode;
+import org.re.common.exception.DomainException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,12 +35,34 @@ public class AppExceptionAdvice {
             .body(body);
     }
 
+    @ExceptionHandler(DomainException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDomainException(DomainException e) {
+        return new ErrorResponse(ErrorResponse.ErrorData.of(e));
+    }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public Object handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         log.error(e.getMessage(), e);
 
         var ae = new AppException(CommonAppExceptionCode.INVALID_HTTP_MESSAGE, e);
         return handleAppException(ae);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleAccessDeniedException() {
+        throw new AppException(CommonAppExceptionCode.ACCESS_DENIED);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        var value = Optional.ofNullable(ex.getValue())
+            .map(Object::toString)
+            .orElse(null);
+        var code = "invalid_argument";
+        var message = String.format("Invalid argument '%s' with value '%s'", code, value);
+        return new ErrorResponse(new ErrorResponse.ErrorData(code, message));
     }
 
     @ExceptionHandler(RuntimeException.class)
