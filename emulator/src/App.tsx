07aaa818af5bc0bf6c8bedc0f2be6d564d13b291
dudlf <dudlf016@gmail.com>
2025-06-30 08:37:20 +0900

    feat: react-rotuer 적용용

--- emulator/src/App.tsx
+++ emulator/src/App.tsx
@@ -1,9 +1,13 @@
-import VehicleSimulator from './components/VehicleSimulator'
+import { Routes, Route } from 'react-router-dom'
+import VehicleSimulator from './routes/emulator'
 
 function App() {
     return (
         <div>
-            <VehicleSimulator/>
+            <Routes>
+                <Route path="/routes/emulator" element={<VehicleSimulator />} />
+                <Route path="/" element={<div>홈페이지입니다. <a href="/routes/emulator">시뮬레이터로 이동</a></div>} />
+            </Routes>
         </div>
     )
 }
