d04e4521e3edd28fe81062d28604b400fc0badba
윤여일 <dudlf016@gmail.com>
2025-07-01 18:08:37 +0900

    Update WebMvcConfig.java

--- hub/src/main/java/org/re/config/WebMvcConfig.java
+++ hub/src/main/java/org/re/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.re.web.resolver.MdtLogRequestTimeInfoResolver;
 import org.re.web.resolver.TransactionUUIDResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
