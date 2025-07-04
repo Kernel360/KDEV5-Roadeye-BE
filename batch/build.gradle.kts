193d6bc2680af3921821e9bedd9ffa4b1005efa5
yooon1109 <jys_1109@naver.com>
2025-07-04 15:42:12 +0900

    refactor: 배치 모듈 의존성 수정

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -4,10 +4,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("mysql:mysql-connector-java:8.0.33")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-    testRuntimeOnly("com.h2database:h2:2.2.220")
-    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.batch:spring-batch-test")
 
