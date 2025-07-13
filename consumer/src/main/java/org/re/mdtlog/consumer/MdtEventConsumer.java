3fc846665d3179171d646d77e4e6fe0f12427bd4
dudlf <dudlf016@gmail.com>
2025-07-13 16:51:15 +0900

    feat!: 주기정보 AMQP Bulk Listener

--- consumer/src/main/java/org/re/mdtlog/consumer/MdtEventConsumer.java
+++ consumer/src/main/java/org/re/mdtlog/consumer/MdtEventConsumer.java
@@ -12,27 +12,39 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class MdtEventConsumer {
     private final MdtEventService mdtEventService;
 
-    @RabbitListener(queues = AMQPConfig.QueueNames.MDT_IGNITION_ON)
+    @RabbitListener(
+        queues = AMQPConfig.QueueNames.MDT_IGNITION_ON,
+        batch = "false"
+    )
     public void handleMdtIgnitionOnMessage(@Payload MdtEventMessage<MdtIgnitionOnMessage> message) {
         log.debug("Received MDT ignition message: {}", message);
         mdtEventService.handleMdtIgnitionOnMessage(message);
     }
 
-    @RabbitListener(queues = AMQPConfig.QueueNames.MDT_IGNITION_OFF)
+    @RabbitListener(
+        queues = AMQPConfig.QueueNames.MDT_IGNITION_OFF,
+        batch = "false"
+    )
     public void handleMdtIgnitionOffMessage(@Payload MdtEventMessage<MdtIgnitionOffMessage> message) {
         log.debug("Received MDT ignition off message: {}", message);
         mdtEventService.handleMdtIgnitionOffMessage(message);
     }
 
-    @RabbitListener(queues = AMQPConfig.QueueNames.MDT_CAR_LOCATION)
-    public void handleMdtCarLocationMessage(@Payload MdtEventMessage<MdtCycleLogMessage> message) {
-        log.debug("Received MDT car location message: {}", message);
-        mdtEventService.handleMdtCarLocationMessage(message);
+    @RabbitListener(
+        queues = AMQPConfig.QueueNames.MDT_CAR_LOCATION,
+        containerFactory = "batchContainerFactory",
+        batch = "true"
+    )
+    public void handleBatchedMdtCarLocationMessage(@Payload List<MdtEventMessage<MdtCycleLogMessage>> batch) {
+        log.debug("[Thread-{}] Received dt car location with batch size: {}", Thread.currentThread(), batch.size());
+        mdtEventService.handleMdtCarLocationMessageBatch(batch);
     }
 }
